80d5cc23a35d7a4c3ea5668e6ac0e74ac9d3927e
Logan <hs.lee.logan@gmail.com>
2020-04-03 17:41:31 +0900

    Deep Mushroom Forest

--- src/map_builders/mushroom_forest.rs
+++ src/map_builders/mushroom_forest.rs
@@ -3,7 +3,7 @@ use super::{
     CullUnreachable, PrefabBuilder, VoronoiSpawning, WaveformCollapseBuilder, XEnd, XStart, YEnd,
     YStart,
 };
-use crate::map_builders::prefab_builder::prefab_sections::UNDERGROUND_FORT;
+use crate::map_builders::prefab_builder::prefab_sections::*;
 
 pub fn mushroom_entrance(
     new_depth: i32,
@@ -20,5 +20,40 @@ pub fn mushroom_entrance(
     chain.with(AreaEndingPosition::new(XEnd::LEFT, YEnd::CENTER));
     chain.with(VoronoiSpawning::new());
     chain.with(PrefabBuilder::sectional(UNDERGROUND_FORT));
+    chain
+}
+
+pub fn mushroom_builder(
+    new_depth: i32,
+    _rng: &mut rltk::RandomNumberGenerator,
+    width: i32,
+    height: i32,
+) -> BuilderChain {
+    let mut chain = BuilderChain::new(new_depth, width, height, "Into The Mushroom Grove");
+    chain.start_with(CellularAutomataBuilder::new());
+    chain.with(WaveformCollapseBuilder::new());
+    chain.with(AreaStartingPosition::new(XStart::CENTER, YStart::CENTER));
+    chain.with(CullUnreachable::new());
+    chain.with(AreaStartingPosition::new(XStart::RIGHT, YStart::CENTER));
+    chain.with(AreaEndingPosition::new(XEnd::LEFT, YEnd::CENTER));
+    chain.with(VoronoiSpawning::new());
+    chain
+}
+
+pub fn mushroom_exit(
+    new_depth: i32,
+    _rng: &mut rltk::RandomNumberGenerator,
+    width: i32,
+    height: i32,
+) -> BuilderChain {
+    let mut chain = BuilderChain::new(new_depth, width, height, "Into The Mushroom Grove");
+    chain.start_with(CellularAutomataBuilder::new());
+    chain.with(WaveformCollapseBuilder::new());
+    chain.with(AreaStartingPosition::new(XStart::CENTER, YStart::CENTER));
+    chain.with(CullUnreachable::new());
+    chain.with(AreaStartingPosition::new(XStart::RIGHT, YStart::CENTER));
+    chain.with(AreaEndingPosition::new(XEnd::LEFT, YEnd::CENTER));
+    chain.with(VoronoiSpawning::new());
+    chain.with(PrefabBuilder::sectional(DROW_ENTRY));
     chain
 }
--- src/raws/mod.rs
+++ src/raws/mod.rs
@@ -12,6 +12,8 @@ mod faction_structs;
 pub use faction_structs::*;
 mod spell_structs;
 pub use spell_structs::Spell;
+mod weapon_traits;
+pub use weapon_traits::*;
 
 mod rawmaster;
 pub use rawmaster::*;
@@ -33,6 +35,7 @@ pub struct Raws {
     pub loot_tables: Vec<LootTable>,
     pub faction_table: Vec<FactionInfo>,
     pub spells: Vec<Spell>,
+    pub weapon_traits: Vec<WeaponTrait>,
 }
 
 pub fn load_raws() {
